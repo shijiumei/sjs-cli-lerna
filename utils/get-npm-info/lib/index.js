bd649b0731c65474ab44d50c3add487d2d6b857a
zzh <99791885+shijiumei@users.noreply.github.com>
2022-05-25 12:30:47 +0800

    fix

--- utils/get-npm-info/lib/index.js
+++ utils/get-npm-info/lib/index.js
@@ -65,6 +65,7 @@ async function getNpmSemverVersion(baseVersion, npmName, registry){
     if(newVersions && newVersions.length > 0){
         return newVersions[0];
     }
+    return null;
 }
 
 module.exports = {
